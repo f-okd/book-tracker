133f847a6ab12ce03701222f0195c936f817ea11
Faye Okude <oluwafayefunmiokude@gmail.com>
2023-12-01 17:40:44 +0000

    navigate to login page after sign up

--- src/modules/auth/hooks/useSignUp.ts
+++ src/modules/auth/hooks/useSignUp.ts
@@ -1,13 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import { supabaseSignUp } from '../../../services/supabase/apiAuth';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 export const useSignUp = () => {
+  const navigate = useNavigate();
   const { mutate: signUp, isPending: isSigningUp } = useMutation({
     mutationFn: supabaseSignUp,
     onSuccess: () => {
       toast.success(
         "Account successfully created! Please verify the new account from the user's email address",
       );
+      navigate('/', { replace: true });
     },
   });
 
